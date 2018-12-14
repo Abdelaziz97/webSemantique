20fa2da5837573e8077853889f5d380ac20ac792
Abdelaziz97 <eabdelaziz97@gmail.com>
2018-12-14 14:13:41 +0100

    Display Medals

--- js/util.js
+++ js/util.js
@@ -36,6 +36,29 @@ function request(filename, fieldId, athlete, format) {
     });
 }
 
+function requestArray(filename, fieldId, athlete, format) {
+    readTextFile(filename, function(req) {
+        req = req.replace('%ATHLETE%', '"' + athlete + '"');
+        var reqUrl = 'http://dbpedia.org/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ encodeURIComponent(req) +'&format=json';
+        $.getJSON(reqUrl+"&callback=?", function(resultatsReq) {
+            var result = [];
+			for(let i=0;i<resultatsReq.results.bindings.length;++i){
+				result.push(format(resultatsReq.results.bindings[i]));
+			}
+			result.sort().reverse();
+            if (result.length>0) {
+            	for(let i=0;i<resultatsReq.results.bindings.length;++i){
+					var e = $("<li></li>");
+					e.html(result[i]);
+	        		$(fieldId).append(e);
+				}
+            } else {
+                $(fieldId).parent().hide();
+            }
+        });
+    });
+}
+
 function requestImage(filename, fieldId, capital, format) {
     readTextFile(filename, function(req) {
         req = req.replace('%ATHLETE%', '"' + capital + '"');
